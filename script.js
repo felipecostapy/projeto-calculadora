efd003000b3f7a4c4f9bda7690125d7c3a40d692
Felipe Costa <contatolipecosta10@gmail.com>
2025-11-10 16:38:33 -0300

    Finalizado

---
+++ script.js
@@ -0,0 +1,23 @@
+const display = document.getElementById("display");
+
+function adicionar(valor){
+     display.value += valor
+}
+
+function limpar(){
+     display.value = "";
+
+}
+
+function apagar(){
+     display.value = display.value.slice(0, -1)
+}
+
+function calcular(){
+     try{
+          display.value = "ERROR";
+          
+     }catch{
+          display.value = eval(display.value);
+     }
+}
